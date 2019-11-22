d3f93a71ea57b290e15c8d331ddb19b8f132db10
tarbielee <56965064+tarbielee@users.noreply.github.com>
2019-11-22 17:09:51 +0200

    Add files via upload

---
+++ password.js
@@ -0,0 +1,28 @@
+"use strict"
+function passwordIsValid(password) {
+
+    let counter = 0
+
+    var myRegex = newRegExp
+    ((B[A-2])(B[9-2])(B[/d])(B[@*$]){8,})
+    if (password.match(myRegex)) {
+        return("password valid");
+    }
+    else
+    {
+        return("password invalid");
+    }
+} 
+passwordIsValid ("Hello")
+passwordIsValid ("hello_universe")
+passwordIsValid ("Hello_123")
+    try{
+        if(password.Match(l[A-2]/g))=num);
+        if(password.Match(l[a-2]/g))=num);
+    }
+    catch(password) {
+        return error
+
+
+    }
+}
